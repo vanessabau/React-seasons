df5f39fe92c1ff3e67219b776f4692b140ae1048
Vanessa Bautista <vbautista916@icloud.com>
2021-01-03 09:08:41 -0800

    refactoring code

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,39 +2,36 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    //This is the only time we do direct assignment to this.state
-    this.state = { lat: null };
+  state = { lat: null, errorMessage: "" };
 
+  //Lifecycle methods
+  componentDidMount() {
     //As soon as our page is called we immediately try and get the user's location
     window.navigator.geolocation.getCurrentPosition(
-      //Success callback
-      (position) => {
-        //We called setState
-        this.setState({
-          lat: position.coords.latitude,
-          errorMessage: "",
-        });
-      },
-
-      //Failure callback; re-render component and inform user of error
-      (err) => {
-        this.setState({
-          errorMessage: err.message,
-        });
-      }
+      //Success callback or error message
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
     );
   }
 
+  componentDidUpdate() {
+    console.log("My component just updated and re-rendered");
+  }
+
+  //Conditional rendering
   render() {
-    return (
-      <div>
-        Latitude: {this.state.lat}
-        <br />
-        Error: {this.state.errorMessage}
-      </div>
-    );
+    //If we have error message but no latitude return error msg
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    //If I have latitude but no error message return latitude
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+
+    //If there is no lat and no error show loading
+    return <div>Loading...</div>;
   }
 }
 
