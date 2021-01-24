ac4c8f61c23cebe6bc42805766ffa313cf836f11
vanessabau <vbautista916@icloud.com>
2021-01-24 09:08:11 -0800

    Update index.js

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,38 +1,45 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  //   //This is the only time we do direct assignment to this.state
+  //   this.state = { lat: null };
+  // }
   state = { lat: null, errorMessage: "" };
-
-  //Lifecycle methods
   componentDidMount() {
     //As soon as our page is called we immediately try and get the user's location
     window.navigator.geolocation.getCurrentPosition(
-      //Success callback or error message
-      (position) => this.setState({ lat: position.coords.latitude }),
-      (err) => this.setState({ errorMessage: err.message })
+      //Success callback
+      //Get latitude out of our position object and update the latitude by calling setState; pass it an object containing the update I want to make to setState
+      (position) =>
+        this.setState({
+          lat: position.coords.latitude,
+        }),
+      //Failure callback; re-render component and inform user of error
+      (err) => {
+        this.setState({
+          errorMessage: err.message,
+        });
+      }
     );
   }
 
-  componentDidUpdate() {
-    console.log("My component just updated and re-rendered");
-  }
-
-  //Conditional rendering
   render() {
-    //If we have error message but no latitude return error msg
-    if (this.state.errorMessage && !this.state.lat) {
-      return <div>Error: {this.state.errorMessage}</div>;
-    }
-
-    //If I have latitude but no error message return latitude
-    if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
-    }
-
-    //If there is no lat and no error show loading
-    return <div>Loading...</div>;
+    return (
+      <div>
+        <SeasonDisplay lat={this.state.lat} />
+        {/* Latitude: {this.state.lat} */}
+        <br />
+        {this.state.errorMessage}
+      </div>
+    );
   }
 }
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(
+  <App />,
+  document.querySelector("#root")
+);
