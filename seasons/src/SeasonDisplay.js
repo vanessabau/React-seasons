4c36967cb7223cbb205af434b34c95a213264a95
vanessabau <vbautista916@icloud.com>
2021-01-24 09:09:01 -0800

    Create SeasonDisplay.js

---
+++ seasons/src/SeasonDisplay.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+const seasonConfig = {
+  summer: {
+    text: "Let's hit the beach!",
+    iconName: "sun",
+  },
+  winter: {
+    text: "Burr it's cold!",
+    iconName: "snowflake",
+  },
+};
+
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summer";
+  }
+};
+
+const SeasonDisplay = (props) => {
+  const season = getSeason(
+    props.lat,
+    new Date().getMonth()
+  );
+  //Destructure out vaues from the seasonConfig object to use in the return <div>
+  const { text, iconName } = seasonConfig[season];
+
+  return (
+    <div>
+      <i className={`${iconName} icon`} />
+      <h1>{text}</h1>
+      <i className={`${iconName} icon`} />
+    </div>
+  );
+};
+
+export default SeasonDisplay;
